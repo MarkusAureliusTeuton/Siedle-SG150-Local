9aaaef3771b2150e93ce632bf87e068e8c06e843
MarkusAureliusTeuton <mberzl@gmx.net>
2026-09-18 11:16:43 +0200

    Add custom_components/sg150_local/sensor.py

---
+++ custom_components/sg150_local/sensor.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+
+from .const import DOMAIN, RUNTIME_CONTROLLER
+from .controller import SG150TabletController
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddConfigEntryEntitiesCallback,
+) -> None:
+    controller: SG150TabletController = hass.data[DOMAIN][entry.entry_id][
+        RUNTIME_CONTROLLER
+    ]
+    async_add_entities([SG150TabletStatusSensor(entry, controller)])
+
+
+class SG150TabletStatusSensor(SensorEntity):
+    _attr_has_entity_name = True
+    _attr_name = "Tabletsteuerung"
+    _attr_icon = "mdi:tablet-dashboard"
+
+    def __init__(
+        self, entry: ConfigEntry, controller: SG150TabletController
+    ) -> None:
+        self._controller = controller
+        self._attr_unique_id = f"{entry.entry_id}_tablet_status"
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, entry.entry_id)},
+            name="Siedle SG150",
+            manufacturer="Siedle",
+            model="SG 150-0",
+        )
+        self._remove_listener = None
+
+    @property
+    def native_value(self) -> str:
+        if not self._controller.configured:
+            return "nicht konfiguriert"
+        return self._controller.state
+
+    @property
+    def extra_state_attributes(self) -> dict:
+        return {
+            "automatic": self._controller.auto_enabled,
+            "last_action": self._controller.last_action,
+            "last_error": self._controller.last_error or None,
+        }
+
+    async def async_added_to_hass(self) -> None:
+        @callback
+        def _update() -> None:
+            self.async_write_ha_state()
+
+        self._remove_listener = self._controller.add_listener(_update)
+
+    async def async_will_remove_from_hass(self) -> None:
+        if self._remove_listener is not None:
+            self._remove_listener()
+            self._remove_listener = None
